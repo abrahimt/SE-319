5b67575484987e2914d1798ee28601f16f13c6c4
maxmayermader <maxmayermader2@gmail.com>
2023-03-28 14:48:26 -0500

    second commit

--- flask_webserver/main.py
+++ flask_webserver/main.py
@@ -3,16 +3,32 @@
 from flask import Flask, render_template
 import RPi.GPIO as GPIO
 import Adafruit_DHT as dht
+import os
  
 app = Flask(__name__)
+
+IMG_FOLDER = os.path.join('static', 'img')
+app.config['UPLOAD_FOLDER'] = IMG_FOLDER
+
  
 GPIO.setmode(GPIO.BCM)
 DHT11_pin = 4
  
 @app.route("/")
- 
 def main():
    return render_template('index.html')
+
+@app.route("/about.html")
+def about():
+    return render_template('about.html')
+
+@app.route("/project.html")
+def project():
+    return render_template('project.html')
+
+@app.route("/countries.html")
+def countries():
+    return render_template('countries.html')
  
 # The function below is executed when someone requests a URL with the pin number and action in it:
 @app.route("/<pin>/<action>")
@@ -32,7 +48,10 @@ def action(pin, action):
    'humidity' : humidity
    }
  
-   return render_template('index.html', **templateData)
+   return render_template('sensor.html', **templateData)
  
-if __name__ == "__main__":
+if __name__ == ("__main__"):
    app.run(host='0.0.0.0', port=80, debug=True)
+
+
+
